319455ee66a8f465a7493fb2e473f9dbf6ec7627
Gavin <72846934+gav06@users.noreply.github.com>
2021-02-22 22:33:05 -0500

    added packet events

---
+++ src/main/java/me/gavin/mimeware/mixin/mixins/MixinNetworkManager.java
@@ -0,0 +1,33 @@
+package me.gavin.mimeware.mixin.mixins;
+
+import io.netty.channel.ChannelHandlerContext;
+import me.gavin.mimeware.Mimeware;
+import me.gavin.mimeware.mimeware.events.PacketEvent;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.Packet;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(NetworkManager.class)
+public class MixinNetworkManager {
+
+    Mimeware mimeware = Mimeware.getMimeware();
+
+    @Inject(method = "sendPacket(Lnet/minecraft/network/Packet;)V", at = @At("HEAD"), cancellable = true)
+    public void onPacketSend(Packet<?> packet, CallbackInfo ci) {
+        PacketEvent.Send packetEvent = new PacketEvent.Send(packet);
+        mimeware.EVENT_BUS.post(packetEvent);
+        if (packetEvent.isCancelled())
+            ci.cancel();
+    }
+
+    @Inject(method = "channelRead0", at = @At("HEAD"), cancellable = true)
+    public void onPacketRead(ChannelHandlerContext chc, Packet<?> packet, CallbackInfo ci) {
+        PacketEvent.Receive packetEvent = new PacketEvent.Receive(packet);
+        mimeware.EVENT_BUS.post(packetEvent);
+        if (packetEvent.isCancelled())
+            ci.cancel();
+    }
+}
